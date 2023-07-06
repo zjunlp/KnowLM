8efc89939af090e102a3a71eb99bf01a965863ab
Jintian Zhang <65744560+MikeDean2367@users.noreply.github.com>
2023-07-06 10:33:29 +0800

    Update download.py

--- tools/download.py
+++ tools/download.py
@@ -13,7 +13,8 @@ def add_argument():
     parser.add_argument('--only_base', action='store_true', default=False)
     parser.add_argument('--fp16', action='store_true', default=False)
     parser.add_argument('--both', action='store_true', default=False)
-    parser.add_argument('--llama', action='store_true', default=False)
+    parser.add_argument('--specify', action='store_true', default=False)
+    parser.add_argument('--repo_name', type=str, default="decapoda-research/llama-13b-hf")
     args = parser.parse_args()
     return args
 
@@ -21,24 +22,25 @@ def check_args(args):
     assert args.only_lora or args.only_base or args.both, \
         "Please select the file to download."
     assert (args.only_lora and not args.only_base and not args.both) \
-        or (not args.only_lora and args.only_base and not args.both) \
-        or (not args.only_lora and not args.only_base and args.both), \
+           or (not args.only_lora and args.only_base and not args.both) \
+           or (not args.only_lora and not args.only_base and args.both), \
         "args conflict."
 
 
 if __name__ == '__main__':
     args = add_argument()
-    if args.llama:
-        snapshot_download(repo_id=f"decapoda-research/llama-13b-hf", local_dir="./llama-13b")
+    if args.specify:
+        _print(f"downloading {args.repo_name}......")
+        snapshot_download(repo_id=args.repo_name, local_dir=args.download_path)
         _print("done!")
         exit()
     check_args(args)
 
     download = []
-    if args.only_base or args.both: 
-        if args.fp16: 
+    if args.only_base or args.both:
+        if args.fp16:
             download.append("ZhiXi-13B-Diff-fp16")
-        else: 
+        else:
             download.append("ZhiXi-13B-Diff")
     if args.only_lora or args.both: download.append("ZhiXi-13B-LoRA")
 
@@ -46,5 +48,3 @@ if __name__ == '__main__':
         _print(f"downloading {file}......")
         snapshot_download(repo_id=f"zjunlp/{file}", local_dir=args.download_path)
     _print("done!")
-    
-    # python ../diff.py recover --path_raw ../converted_llama13 --path_diff zjunlp/CaMA-13B --path_tuned ./recover
