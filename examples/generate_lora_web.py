4491f33cc0103e5ead03887f7f4a6364547c9024
MikeDean2367 <MikeDean2367@outlook.com>
2023-12-06 17:10:52 +0800

    fix issue #99,

--- examples/generate_lora_web.py
+++ examples/generate_lora_web.py
@@ -5,7 +5,7 @@ import fire
 import gradio as gr
 import torch
 import transformers
-# from peft import PeftModel
+from peft import PeftModel
 from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
 from multi_gpu_inference import get_tokenizer_and_model
 from typing import List
@@ -26,7 +26,7 @@ except:  # noqa: E722
 
 
 def main(
-    load_8bit: bool = False,
+    load_8bit: bool = True,
     base_model: str = None,
     # lora_weights: str = "zjunlp/CaMA-13B-LoRA",
     prompt_template: str = "finetune/lora/knowlm/templates/alpaca.json",  # The prompt template to use, will default to alpaca.
@@ -46,11 +46,15 @@ def main(
         if multi_gpu:
             model, tokenizer = get_tokenizer_and_model(base_model=base_model, dtype="float16", allocate=allocate)
         else:
+            if load_8bit:
+                device_map = {"":0}
+            else:
+                device_map = {"": device}
             model = LlamaForCausalLM.from_pretrained(
                 base_model,
                 load_in_8bit=load_8bit,
                 torch_dtype=torch.float16,
-                device_map={"": device},
+                device_map=device_map,
             )
         # model = PeftModel.from_pretrained(
         #     model,
