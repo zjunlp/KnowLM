8a9056714f1a23796efbef882da95ea0c40227f8
MikeDean2367 <MikeDean2367@outlook.com>
2024-04-17 15:11:12 +0800

    support  model

--- examples/generate_lora_web.py
+++ examples/generate_lora_web.py
@@ -6,12 +6,13 @@ import gradio as gr
 import torch
 import transformers
 from peft import PeftModel
-from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+from transformers import GenerationConfig, AutoModelForCausalLM, AutoTokenizer
 from multi_gpu_inference import get_tokenizer_and_model
 from typing import List
 
 from callbacks import Iteratorize, Stream
 from prompter import Prompter
+from utils import Web
 
 if torch.cuda.is_available():
     device = "cuda"
@@ -24,25 +25,35 @@ try:
 except:  # noqa: E722
     pass
 
+# model = None
+# tokenizer = None
+# web_config = None
+
+def get_kwargs(kwargs, *args):
+    return [kwargs.get(arg, None) for arg in args]
+
 
 def main(
     load_8bit: bool = True,
     load_4bit: bool = False,
     base_model: str = None,
+    model_tag: str = 'zhixi',
     # lora_weights: str = "zjunlp/CaMA-13B-LoRA",
-    prompt_template: str = "finetune/lora/knowlm/templates/alpaca.json",  # The prompt template to use, will default to alpaca.
+    # prompt_template: str = "finetune/lora/knowlm/templates/alpaca.json",  # The prompt template to use, will default to alpaca.
     server_name: str = "0.0.0.0",  # Allows to listen on all interfaces by providing '0.
     share_gradio: bool = False,
     multi_gpu: bool = False,
     allocate: List[int] = None
 ):
+    # global model
+    # global tokenizer
+    # global web_config
     base_model = base_model or os.environ.get("BASE_MODEL", "")
     assert (
         base_model
     ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
 
-    prompter = Prompter(prompt_template)
-    tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
     if device == "cuda":
         if multi_gpu:
             model, tokenizer = get_tokenizer_and_model(base_model=base_model, dtype="float16", allocate=allocate)
@@ -53,7 +64,7 @@ def main(
                 device_map = {"": device}
             if load_4bit:
                 load_8bit = False
-            model = LlamaForCausalLM.from_pretrained(
+            model = AutoModelForCausalLM.from_pretrained(
                 base_model,
                 load_in_4bit=load_8bit,
                 load_in_8bit=load_4bit,
@@ -66,7 +77,7 @@ def main(
         #     torch_dtype=torch.float16,
         # )
     elif device == "mps":
-        model = LlamaForCausalLM.from_pretrained(
+        model = AutoModelForCausalLM.from_pretrained(
             base_model,
             device_map={"": device},
             torch_dtype=torch.float16,
@@ -78,12 +89,12 @@ def main(
         #     torch_dtype=torch.float16,
         # )
     elif device == 'cpu':
-        model = LlamaForCausalLM.from_pretrained(
+        model = AutoModelForCausalLM.from_pretrained(
             base_model,
             torch_dtype=torch.float32,
         )
     else:
-        model = LlamaForCausalLM.from_pretrained(
+        model = AutoModelForCausalLM.from_pretrained(
             base_model, device_map={"": device}, low_cpu_mem_usage=True
         )
         # model = PeftModel.from_pretrained(
@@ -92,10 +103,18 @@ def main(
         #     device_map={"": device},
         # )
 
+
+    if 'oneke' in base_model.lower():
+        model_tag = 'oneke'
+    elif 'knowlm' in base_model.lower():
+        model_tag = 'zhixi'
+    assert model_tag in  Web.__SUPPORT_MODEL__
+    web_config = Web.get_ui(model_tag)
+    prompter = Prompter(model_name=model_tag)
     # unwind broken decapoda-research config
-    model.config.pad_token_id = tokenizer.pad_token_id = 0  # pad
-    model.config.bos_token_id = tokenizer.pad_token_id = 1
-    model.config.eos_token_id = tokenizer.pad_token_id = 2
+    # model.config.pad_token_id = tokenizer.pad_token_id = 0  # pad
+    # model.config.bos_token_id = tokenizer.pad_token_id = 1
+    # model.config.eos_token_id = tokenizer.pad_token_id = 2
 
     if not load_8bit and device != "cpu":
         model.half()  # seems to fix bugs for some users.
@@ -104,28 +123,64 @@ def main(
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
 
+
     def evaluate(
-        instruction,
-        input=None,
-        temperature=0.4,
-        top_p=0.75,
-        top_k=40,
-        num_beams=2,
-        max_new_tokens=512,
-        repetition_penalty=1.3,
-        stream_output=False,
-        **kwargs,
+        # instruction,
+        # input=None,
+        # temperature=0.4,
+        # top_p=0.75,
+        # top_k=40,
+        # num_beams=2,
+        # max_new_tokens=512,
+        # repetition_penalty=1.3,
+        # stream_output=False,
+        *args
+        # **kwargs,
     ):
-        prompt = prompter.generate_prompt(instruction, input)
+        kwargs = {}
+        assert len(web_config['var_name']) == len(args), f"{en(web_config['var_name'])} == {len(args)}"
+        for key, value in zip(web_config['var_name'], args):
+            kwargs[key] = value
+        print(kwargs)
+        generation_var = [
+            'temperature', 
+            'top_p', 
+            'top_k', 
+            'num_beams', 
+            'max_new_tokens', 
+            'repetition_penalty',
+            'stream_output'
+        ]
+        input_var = [
+            'instruction', 
+            'input', 
+            'schema',
+            'system_prompt',
+        ]
+
+        all_var = input_var + generation_var
+        instruction, input, schema, system_prompt, \
+            temperature, top_p, top_k, num_beams, max_new_tokens, repetition_penalty, stream_output = get_kwargs(kwargs, *all_var)
+
+        generation_var_args = {}
+        input_var_args = {}
+        for cur_var in [(generation_var, generation_var_args), (input_var, input_var_args)]:
+            _cur_var_list, _cur_arg = cur_var
+            for var_name in _cur_var_list:
+                if var_name in kwargs:
+                    _cur_arg[var_name] = kwargs.pop(var_name)
+        
+        prompt = prompter.generate_prompt(**input_var_args)
         inputs = tokenizer(prompt, return_tensors="pt")
         input_ids = inputs["input_ids"].to(device)
         generation_config = GenerationConfig(
-            temperature=temperature,
-            top_p=top_p,
-            top_k=top_k,
-            num_beams=num_beams,
-            repetition_penalty=repetition_penalty,
-            **kwargs,
+            **generation_var_args
+            # temperature=temperature,
+            # top_p=top_p,
+            # top_k=top_k,
+            # num_beams=num_beams,
+            # repetition_penalty=repetition_penalty,
+            # **kwargs,
         )
 
         generate_params = {
@@ -159,7 +214,7 @@ def main(
             with generate_with_streaming(**generate_params) as generator:
                 for output in generator:
                     # new_tokens = len(output) - len(input_ids[0])
-                    decoded_output = tokenizer.decode(output)
+                    decoded_output = tokenizer.decode(output[1:])   # # `1:` means that we need to filter bos token
 
                     if output[-1] in [tokenizer.eos_token_id]:
                         break
@@ -176,52 +231,54 @@ def main(
                 output_scores=True,
                 max_new_tokens=max_new_tokens,
             )
-        s = generation_output.sequences[0]
-        output = tokenizer.decode(s)
+        s = generation_output.sequences[0]  
+        output = tokenizer.decode(s[1:])    # `1:` means that we need to filter bos token
         yield prompter.get_response(output)
 
-   
+
+
     gr.Interface(
         fn=evaluate,
-        inputs=[
-            gr.components.Textbox(
-                lines=2,
-                label="Instruction",
-                placeholder="<请在此输入你的问题>",
-            ),
-            gr.components.Textbox(
-                lines=2, 
-                label="Input", 
-                placeholder="<可选参数>",
-            ),
-            gr.components.Slider(
-                minimum=0, maximum=1, value=0.4, label="Temperature"
-            ),
-            gr.components.Slider(
-                minimum=0, maximum=1, value=0.75, label="Top p"
-            ),
-            gr.components.Slider(
-                minimum=0, maximum=100, step=1, value=40, label="Top k"
-            ),
-            gr.components.Slider(
-                minimum=1, maximum=4, step=1, value=2, label="Beams"
-            ),
-            gr.components.Slider(
-                minimum=1, maximum=2000, step=1, value=512, label="Max tokens"
-            ),
-            gr.components.Slider(
-                minimum=1, maximum=2, step=0.1, value=1.3, label="Repetition Penalty"
-            ),
-            gr.components.Checkbox(label="Stream output"),
-        ],
+        inputs=web_config['components'],
+        # inputs=[
+        #     gr.components.Textbox(
+        #         lines=2,
+        #         label="Instruction",
+        #         placeholder="<请在此输入你的问题>",
+        #     ),
+        #     gr.components.Textbox(
+        #         lines=2, 
+        #         label="Input", 
+        #         placeholder="<可选参数>",
+        #     ),
+        #     gr.components.Slider(
+        #         minimum=0, maximum=1, value=0.4, label="Temperature"
+        #     ),
+        #     gr.components.Slider(
+        #         minimum=0, maximum=1, value=0.75, label="Top p"
+        #     ),
+        #     gr.components.Slider(
+        #         minimum=0, maximum=100, step=1, value=40, label="Top k"
+        #     ),
+        #     gr.components.Slider(
+        #         minimum=1, maximum=4, step=1, value=2, label="Beams"
+        #     ),
+        #     gr.components.Slider(
+        #         minimum=1, maximum=2000, step=1, value=512, label="Max tokens"
+        #     ),
+        #     gr.components.Slider(
+        #         minimum=1, maximum=2, step=0.1, value=1.3, label="Repetition Penalty"
+        #     ),
+        #     gr.components.Checkbox(label="Stream output"),
+        # ],
         outputs=[
             gr.Textbox(
                 lines=5,
                 label="Output",
             )
         ],
-        title="智析",
-        description="智析（ZhiXi）是基于LLaMA-13B，先使用中英双语进行全量预训练，然后使用指令数据集进行LoRA微调（我们专门针对信息抽取进行优化）。如果希望获得更多信息，请参考[KnowLM](https://github.com/zjunlp/knowlm)。如果出现重复或者效果不佳，请调整repeatition_penalty、beams两个参数。",  # noqa: E501
+        title=web_config['title'],
+        description=web_config['description'],
     ).queue().launch(server_name="0.0.0.0", share=share_gradio)
 
 
@@ -232,4 +289,6 @@ if __name__ == "__main__":
     CUDA_VISIBLE_DEVICES=0,1,2,3 python examples/generate_lora_web.py --base_model zjunlp/knowlm-13b-zhixi --multi_gpu --allocate [5,10,8,10]
     # single-gpu
     CUDA_VISIBLE_DEVICES=0,1,2,3 python examples/generate_lora_web.py --base_model zjunlp/knowlm-13b-zhixi
+
+    CUDA_VISIBLE_DEVICES=0,1 python examples/generate_lora_web.py --base_model /disk/disk_20T/data/guihh/datasets/modelscope/OneKE --multi_gpu --allocate [16,16]
     """
